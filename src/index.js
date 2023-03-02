9364f64acbe8b526ec7b20705b222bff656e14f3
Tanya Landsman <tlandsman@activecampaign.com>
2023-03-02 08:42:09 -0600

    update

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "odyssey-lift-off-part5-server-production-84e2.up.railway.app", // change to YOUR own production server
+  uri: "https://odyssey-lift-off-part5-server-production-84e2.up.railway.app/", // change this to YOUR server URI
   cache: new InMemoryCache(),
-  name: "web",
-  version: "1.0",
 });
 
 ReactDOM.render(
